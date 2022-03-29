03f4c5f7fe6b6d1f32b706e26002c2068200a49d
Apl064 <apl064@gmail.com>
2022-03-29 17:10:18 +0300

    Энум и первый экран

--- CalendarOfLifeEvents/СalendarOfLifeEvents/СalendarOfLifeEvents/ViewControllers/IntroViewController.swift
+++ CalendarOfLifeEvents/СalendarOfLifeEvents/СalendarOfLifeEvents/ViewControllers/IntroViewController.swift
@@ -15,7 +15,6 @@ class IntroViewController: UIViewController {
         }
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
